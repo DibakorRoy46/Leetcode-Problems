e9bfb00f8efb0f706e4f5aeacff137227317f629
Dibakor Roy <dibakorroy56@gmail.com>
2025-01-10 22:32:44 +0600

    Submission MoveZeroes

---
+++ LeetcodeProblems/LeetcodeProblems/MoveZeroes_283.cs
@@ -0,0 +1,51 @@
+﻿
+namespace LeetcodeProblems
+{
+    public static class MoveZeroes_283
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            Approch1(nums);
+            Approch2(nums);
+        }
+
+        public static void Approch1(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[count];
+                    nums[count] = temp;
+                    count++;
+                }
+            }
+        }
+
+        public static void Approch2(int[] nums)
+        {
+            List<int> zeroList = new List<int>();
+            List<int> result= new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0 )
+                {
+                    zeroList.Add(0);
+                }
+                else
+                { 
+                    result.Add(nums[i]);
+                }
+            }
+
+            result.AddRange(zeroList);
+            for(int i = 0;i < result.Count;i++)
+            {
+                nums[i] = result[i];
+            }
+        }
+
+    }
+}
---
+++ LeetcodeProblems/LeetcodeProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetcodeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [0, 1, 0, 3, 12];
+            MoveZeroes_283.MoveZeroes(nums);
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);   
+            }
+        }
+    }
+}
