43e8b1902b98c0e63c1e5419512b0ddf983715e3
Dibakor Roy <dibakorroy56@gmail.com>
2025-01-11 22:05:56 +0600

    Submit Majority Element 169

---
+++ LeetcodeProblems/LeetcodeProblems/MajorityElement_169.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeProblems
+{
+    public static class MajorityElement_169
+    {
+        //int[] nums = [3, 2, 3,2,2,2];
+        public static int MajorityElement(int[] nums)
+        {
+            return Approch1(nums); 
+        }
+
+        public static int Approch1(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], 1);
+                }
+                else
+                {
+                    dic[nums[i]]++;
+                }
+            }
+
+            int max = 0;
+            int result = int.MinValue ;
+
+            foreach (int item in dic.Keys)
+            {
+                if (dic[item] > max)
+                {
+                    max = dic[item];
+                    result = item;
+                }
+            }
+
+            return result;
+        }
+
+
+        public static int Approch2(int[] nums)
+        {
+            int count = 1;
+            int index = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[index] == nums[i])
+                    count++;
+                else
+                    count--;
+                if(count == 0)
+                {
+                    count = 1;
+                    index = i;
+                }
+            }
+
+            count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[index] == nums[i])
+                    count++;
+            }
+
+            return count > nums.Length / 2 ? nums[index] : -1;
+        }
+    }
+}
--- LeetcodeProblems/LeetcodeProblems/Program.cs
+++ LeetcodeProblems/LeetcodeProblems/Program.cs
@@ -4,13 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int[] nums = [0, 1, 0, 3, 12];
-            MoveZeroes_283.MoveZeroes(nums);
 
-            foreach (int i in nums)
-            {
-                Console.WriteLine(i);   
-            }
+            #region MoveZeros 283
+
+            //int[] nums = [0, 1, 0, 3, 12];
+            //MoveZeroes_283.MoveZeroes(nums);
+
+            #endregion
+
+
+            #region Majority Element 169
+            //int[] nums = [3, 2, 3];
+            //int result=MajorityElement_169.MajorityElement(nums);
+
+            //Console.WriteLine(result);
+
+            #endregion
+
         }
     }
 }
