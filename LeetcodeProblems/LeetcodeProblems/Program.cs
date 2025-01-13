da79d87cd5e6e4e370f0643f131d069cd001d3c7
Dibakor Roy <dibakorroy56@gmail.com>
2025-01-13 22:14:47 +0600

    Submission 189. Rotate Array

--- LeetcodeProblems/LeetcodeProblems/Program.cs
+++ LeetcodeProblems/LeetcodeProblems/Program.cs
@@ -5,14 +5,14 @@
         static void Main(string[] args)
         {
 
-            #region MoveZeros 283
+            #region 283. MoveZeros
 
             //int[] nums = [0, 1, 0, 3, 12];
             //MoveZeroes_283.MoveZeroes(nums);
 
             #endregion
 
-            #region Majority Element 169
+            #region 169. Majority Element 
             //int[] nums = [3, 2, 3];
             //int result=MajorityElement_169.MajorityElement(nums);
 
@@ -20,7 +20,7 @@
 
             #endregion
 
-            #region Remove Duplicates from Sorted Array 26
+            #region 26. Remove Duplicates from Sorted Array 
 
             //int[] nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
             //Console.WriteLine(RemoveDuplicate_26.RemoveDuplicates(nums));
@@ -29,12 +29,28 @@
 
             #region 121. Best Time to Buy and Sell Stock
 
-            int[] nums = [7, 1, 5, 3, 6, 4];
-            Console.WriteLine(BestTimeBuySellStock_121.MaxProfit(nums));
+            //int[] nums = [7, 1, 5, 3, 6, 4];
+            //Console.WriteLine(BestTimeBuySellStock_121.MaxProfit(nums));
+
+            #endregion
+
+            #region 189. Rotate Array
+
+            //int[] nums = [1, 2, 3, 4, 5, 6, 7];
+            ////int[] nums = [-1, -100, 3, 99];
+            ////int[] nums = [1, 2];
+            //int k = 3;
+            //RotateArray_189.Rotate(nums, k);
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //   Console.WriteLine(nums[i]);
+            //}
 
             #endregion
 
 
         }
+
     }
 }
---
+++ LeetcodeProblems/LeetcodeProblems/RotateArray_189.cs
@@ -0,0 +1,68 @@
+﻿
+namespace LeetcodeProblems
+{
+    public static class RotateArray_189
+    {
+        public static void Rotate(int[] nums, int k)
+        {
+            //Approch1(nums, k);
+            Approch2(nums, k);
+        }
+
+        public static void Approch1(int[] nums, int k)
+        {
+            if (nums.Count() < k)
+                k= nums.Count();
+
+            List<int> firstPhase= new List<int>();
+            List<int> lastPhase= new List<int>();
+            
+            for (int i = nums.Length - k; i <nums.Length; i++)
+            {
+                firstPhase.Add(nums[i]);
+            }
+            for (int i = 0; i < nums.Length-k; i++)
+            {
+                lastPhase.Add(nums[i]);
+            }
+
+            firstPhase.AddRange(lastPhase);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = firstPhase[i];
+            }
+        }
+
+
+        public static void Approch2(int[] nums, int k)
+        {
+            k=k%nums.Length;    
+
+            Reverse(nums,0,nums.Length-1-k);
+            Reverse(nums,nums.Length-k,nums.Length-1);
+            Reverse(nums,0,nums.Length-1);
+        }
+
+        public static void Reverse(int[] nums, int min, int max)
+        {
+            while (min <= max)
+            {
+                int temp = nums[min];
+                nums[min] = nums[max];
+                nums[max] = temp;
+                min++;
+                max--;
+            }
+        }
+
+        /*
+        
+        [1, 2, 3, 4, 5, 6, 7]
+        [4,3,2,1] [7,6,5]
+
+
+
+        */
+    }
+}
