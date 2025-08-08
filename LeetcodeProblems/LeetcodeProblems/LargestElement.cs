486d2a94ad63b6408195ec5a75d9a6213ed78df3
Dibakor Roy <dibakorroy56@gmail.com>
2025-08-08 20:49:16 +0600

    Found Larges and Second Largest

---
+++ LeetcodeProblems/LeetcodeProblems/LargestElement.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetcodeProblems
+{
+    public static class LargestElement
+    {
+        public static int FoundLargestElement(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+            int largest = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > largest)
+                {
+                    largest = nums[i];
+                }
+            }
+            return largest;
+        }
+    }
+}
---
+++ LeetcodeProblems/LeetcodeProblems/SecondLargest.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace LeetcodeProblems
+{
+    public static class SecondLargest
+    {
+        public static int FindSecondLargestElement(int[] nums)
+        {
+            if(nums.Length==0 || nums.Length==1)
+            {
+                return -1; // Not enough elements to find the second largest
+            }
+            int firstLargest = int.MinValue;
+            int secondLargest = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > firstLargest)
+                {
+                    secondLargest = firstLargest;
+                    firstLargest = nums[i];
+                }
+
+                if (nums[i]>secondLargest && nums[i]< firstLargest)
+                {
+                    secondLargest = nums[i];
+                }
+            }
+
+            return secondLargest == int.MinValue ? -1 : secondLargest; // Return -1 if no second largest found
+        }
+    }
+}
