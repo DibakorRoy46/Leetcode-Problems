6134f8c02e8a1769e992d682166c3ace396e285a
Dibakor Roy <dibakorroy56@gmail.com>
2025-08-08 21:32:06 +0600

    Solve Array Sorted Or Not

---
+++ LeetcodeProblems/LeetcodeProblems/ArraySortedOrNot.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LeetcodeProblems
+{
+    public static class ArraySortedOrNot
+    {
+        public static bool IsSortedOrNot(int[] nums)
+        {
+            if (nums.Length==0  || nums.Length==1 )
+            {
+                return true;
+            }
+            for(int i=0;i<nums.Length-1;i++)
+            {
+                if (nums[i] > nums[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
