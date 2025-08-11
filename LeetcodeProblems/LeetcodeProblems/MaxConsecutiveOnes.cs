2f1743cefc4e252aed8d7457d00acdb01d2a8f9d
Dibakor Roy <dibakorroy56@gmail.com>
2025-08-11 09:12:57 +0600

    FindMaxConsecutiveOnes

---
+++ LeetcodeProblems/LeetcodeProblems/MaxConsecutiveOnes.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace LeetcodeProblems
+{
+    public static class MaxConsecutiveOnes
+    {
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0;
+
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    max=Math.Max(max, count);
+                    count = 0; // Reset count when a 0 is encountered
+                }
+            }
+            max = Math.Max(max, count); // Check at the end in case the array ends with 1s
+            return max;
+        }
+    }
+}
