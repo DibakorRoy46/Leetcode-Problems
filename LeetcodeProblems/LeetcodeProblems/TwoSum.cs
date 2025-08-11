523110e95ca6e2b1c361d261eb6af124be1804d6
Dibakor Roy <dibakorroy56@gmail.com>
2025-08-11 09:01:42 +0600

    Add Two Sum

---
+++ LeetcodeProblems/LeetcodeProblems/TwoSum.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LeetcodeProblems
+{
+    public static class TwoSum
+    {
+        public static int[] FindTwoSum(int[] nums,int target)
+        {
+            Dictionary<int, int> numIndices = new Dictionary<int, int>();
+
+            for(int i=0;i<nums.Length;i++)
+            {
+                int needValue=target - nums[i];
+                if(numIndices.ContainsKey(needValue))
+                {
+                    return new int[] { numIndices[needValue], i };
+                }
+                else if(!numIndices.ContainsKey(nums[i]))
+                {
+                    numIndices[nums[i]] = i;
+                }
+            }
+            return new int[] { -1, -1 }; // Return -1, -1 if no solution is found
+        }
+
+        public static int[] FindTwoSum_2(int[] nums, int target)
+        {
+            for(int i=0;i<nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    
+                    if(i!=j && nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { -1, -1 }; // Return -1, -1 if no solution is found
+        }
+    }
+}
