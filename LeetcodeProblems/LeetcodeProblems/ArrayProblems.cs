9e1cdbff66611c07a3b7b86e8957df388512b24a
Dibakor Roy <dibakorroy56@gmail.com>
2025-11-09 09:28:52 +0600

    Add Valid Anagarm

---
+++ LeetcodeProblems/LeetcodeProblems/ArrayProblems.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace LeetcodeProblems;
+
+public static class ArrayProblems
+{
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (charCount.ContainsKey(c))
+                charCount[c]++;
+            else
+                charCount[c] = 1;
+        }
+
+        foreach (char c in t)
+        {
+            if (charCount.ContainsKey(c))
+            {
+                charCount[c]--;
+                if (charCount[c] < 0)
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
